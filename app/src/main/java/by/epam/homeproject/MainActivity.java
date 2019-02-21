7e2c8b1aa7211316cec9b1a1494df53a5b61a098
AndreyIIT4 <andruha_kar@mail.ru>
2019-02-21 20:54:08 +0300

    read toast

--- app/src/main/java/by/epam/homeproject/MainActivity.java
+++ app/src/main/java/by/epam/homeproject/MainActivity.java
@@ -13,4 +13,6 @@ public class MainActivity extends AppCompatActivity {
     }
     Toast toast = Toast.makeText(getApplicationContext(),
             "Пора покормить кота!", Toast.LENGTH_SHORT);
+    Toast toast2 = Toast.makeText(getApplicationContext(),
+            "Пора покормить кота!", Toast.LENGTH_SHORT);
 }
